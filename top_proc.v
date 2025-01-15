b2983f36b79972b53ee173f6d9e5b2ce23910c77
giorgosskl <sklavenitisgeorge21@gmail.com>
2025-01-15 20:09:32 +0200

    procedures fix

--- top_proc.v
+++ top_proc.v
@@ -1,6 +1,4 @@
 `include "datapath.v"
-`include "ram.v"
-`include "rom.v"
 
 module procedures #(
   parameter [31:0] INITIAL_PC = 32'h00400000
@@ -16,27 +14,18 @@ module procedures #(
   output reg MemWrite,
   output wire [31:0] WriteBackData
 );
-  
-  wire [8:0] address = dAddress;
-  DATA_MEMORY ram(
-    .clk(clk),
-    .we(MemWrite),
-    .addr(address),
-    .din(dWriteData),
-    .dout(dReadData)
-);
-  
+
   wire Zero;
-  reg IFStage = 0, IDStage = 0, EXStage = 0, MEMStage = 0, WBStage = 0;
+  reg IFStage, IDStage, EXStage, MEMStage, WBStage;
   
-  reg [4:0] FSM = 4'b0000;
-  reg PCSrc = 0;
-  reg ALUSrc = 0;
-  reg RegWrite = 0;
-  reg MemToReg = 0;
-  reg loadPC = 0;
+  reg [2:0] FSM;
+  reg PCSrc;
+  reg ALUSrc;
+  reg RegWrite;
+  reg MemToReg;
+  reg loadPC;
   
-  reg [3:0] ALUCtrl = 4'b0000;
+  reg [3:0] ALUCtrl;
   
   datapath #(.INITIAL_PC(INITIAL_PC)) datapath(
     .clk(clk), 
@@ -62,7 +51,7 @@ module procedures #(
   parameter [2:0] MEM = 3'b011;
   parameter [2:0] WB = 3'b100;
     
-  wire opcode = instr[6:0];
+  wire [6:0] opcode = instr[6:0];
   parameter [6:0] IMMEDIATE = 7'b0010011;
   parameter [6:0] NON_IMMEDIATE = 7'b0110011;
   parameter [6:0] LW = 7'b0000011;
@@ -73,13 +62,11 @@ module procedures #(
   parameter [2:0] ARITHMETIC = 3'b000;
   parameter [2:0] SLT = 3'b010;
   parameter [2:0] XOR = 3'b100;
-  parameter [2:0]  OR = 3'b110;
+  parameter [2:0] OR = 3'b110;
   parameter [2:0] AND = 3'b111;
   parameter [2:0] SLL = 3'b001;
   parameter [2:0] SRL = 3'b101;
   parameter [2:0] SRA = 3'b101;
-  parameter [2:0] MEMORY_TRANSACTION = 3'b010;
-  parameter [2:0] BRANCH = 3'b000;
   
   parameter [3:0] ALUOP_AND = 4'b0000;
   parameter [3:0] ALUOP_OR  = 4'b0001;
@@ -90,39 +77,56 @@ module procedures #(
   parameter [3:0] ALUOP_SLL = 4'b1001; 
   parameter [3:0] ALUOP_SRA = 4'b1010;  
   parameter [3:0] ALUOP_XOR = 4'b0101;  
-  
-  always @(posedge clk) begin
-      if ( FSM == WBStage || rst )
-          FSM <= IFStage;
-      else 
-          FSM <= FSM + 1;
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        FSM <= IF;
+        MemRead <= 0;
+        MemWrite <= 0;
+        RegWrite <= 0;
+        MemToReg <= 0;
+        loadPC <= 0;
+        ALUSrc <= 0;
+        PCSrc <= 0;
+    end else begin
+        case (FSM)
+            IF: FSM <= ID;
+            ID: FSM <= EX;
+            EX: FSM <= MEM;
+            MEM: FSM <= WB;
+            WB: FSM <= IF;
+        endcase
+    end
   end
 
   always @(posedge clk) begin
-      case ( FSM )
-          IF : begin 
-              {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b10000;
-              loadPC <= 0;
+      case (FSM)
+          IF: begin
+            {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b10000;
+            loadPC <= 0;
           end
-          ID : {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b01000;
-          MEM : {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b00100;
-          EX : {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b00010;
-          WB : begin 
+          ID: {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b01000;
+          MEM: begin
+            {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b00100;
+            if (opcode == LW)
+                MemRead <= 1;
+            else if (opcode == SW)
+                MemWrite <= 1;
+          end
+          EX: {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b00010;
+          WB: begin 
               {IFStage, IDStage, MEMStage, EXStage, WBStage} = 5'b00001;
               loadPC <= 1;
+              if (opcode == LW)
+                  MemToReg <= 1;
+
+              if (opcode != SW && opcode != BEQ)
+                  RegWrite <= 1;
           end
       endcase
   end
-
-  
-  always @(*) begin
-    if (opcode == LW || opcode == SW || opcode == IMMEDIATE)
-      ALUSrc = 1;
-    else
-      ALUSrc = 0;
-  end
         
-  always  @(*) begin
+  always @(*) begin
     if (opcode == NON_IMMEDIATE) begin
       case (funct3)
         ARITHMETIC: begin
@@ -165,43 +169,23 @@ module procedures #(
     else if ( opcode == BEQ )
         ALUCtrl = ALUOP_SUB;
 end
-      
-  always @(posedge MEMStage) begin
-    if (opcode == LW) begin
-      MemRead <= 1;
-      MemWrite <= 0;
-    end
-    else if (opcode == SW) begin
-      MemRead <= 0;
-      MemWrite <= 1;
-    end
-  end
-  
-  always @(posedge WBStage) begin
-    if (opcode == SW || opcode == BEQ)
-      RegWrite = 0;
-    else
-      RegWrite = 1;
-    if (opcode == LW)
-      MemToReg = 1;
-    else
-      MemToReg = 0;
-  end
-  
+
   always @(*) begin
-    if (WBStage == 1) begin
-      loadPC = 1;
+    // ALUSrc  
+    if (opcode == LW || opcode == SW || opcode == IMMEDIATE) begin
+      ALUSrc <= 1;
     end
-    else if (IFStage == 1) begin
-      loadPC = 0;
+    else begin
+      ALUSrc <= 0;
+    end
+    
+    // PCSrc
+    if (opcode == BEQ) begin
+        PCSrc <= Zero ? 1 : 0;
+    end
+    else begin
+        PCSrc <= 0;
     end
   end
-  
-  always @(posedge WBStage) begin
-    if (opcode == BEQ || Zero == 1)
-      PCSrc = 1;
-    else
-      PCSrc = 0;
-  end
-  
+
 endmodule
